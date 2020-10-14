5d487609cd7604639d00cae10d45f3884b9db2fe
softael <softael@outlook.com>
2020-10-13 17:40:33 -0700

    same

--- src/picoc_api.c
+++ src/picoc_api.c
@@ -1,8 +1,26 @@
 
 #include "picoc_api.h"
 
+void MsvcSetupFunc(Picoc *pc)
+{
+}
+
+void picoc_api_speedtest  (struct ParseState *Parser, struct Value *ReturnValue,
+						   struct Value **Param, int NumArgs)
+{
+	for (volatile int i = 0; i < 10000000; ++i) { volatile int a; a = i * i; }
+}
+
+/* list of all library functions and their prototypes */
+struct LibraryFunction MsvcFunctions[] =
+{
+		{picoc_api_speedtest, "void api_speedtest(void);"},
+		{NULL, NULL}
+};
+
 void PlatformLibraryInit(Picoc *pc)
 {
+	IncludeRegister(pc, "api.h", &MsvcSetupFunc, &MsvcFunctions[0], NULL);
 }
 
 
