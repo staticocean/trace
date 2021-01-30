8a49d23130d80a084a61985560feabc52d47f9f3
softael <softael@outlook.com>
2021-01-30 00:04:14 -0800

    [R] [proc] proc euler rework to match finite difference template

--- lib/trj/trj_proc.h
+++ lib/trj/trj_proc.h
@@ -17,20 +17,10 @@
 
 typedef struct trj_proc_euler
 {
-	vlf_t d1p[3];
-	vlf_t d1n[3];
-	vlf_t d2 [3];
+	vlf_t pos[3][3];
+	vlf_t rot[3][9];
 	
-	vlf_t rnt[9]; // rotation next transpose
-	
-	// https://math.stackexchange.com/questions/668866/how-do-you-find-angular-velocity-given-a-pair-of-3x3-rotation-matrices
-	vlf_t a[9];
-	vlf_t at[9];
-	vlf_t da[9];
-	vlf_t tra;
-	vlf_t omg;
-	vlf_t d1rp[9];
-	vlf_t d1rn[9];
+	s_vl_rd1 rd1_data;
 	
 }   s_trj_proc_euler;
 
@@ -59,71 +49,42 @@ inline uint8_t trj_proc_euler_load(s_trj_proc_euler *self, s_trj_proc_euler_init
 
 inline uint8_t trj_proc_euler_update(s_trj_proc_euler *self, s_trj_obj *obj, uint32_t offset)
 {
-	s_trj_obj_data *data = obj->log_list;
+	vlf_t *vel = &obj->log_list[offset].pos[1][0];
+	vlf_t *acc = &obj->log_list[offset].pos[2][0];
 	
-	if (offset > 0x00)
-	{
-		vl_vsub (self->d1p, &data[offset].pos[0][0], &data[offset-1].pos[0][0]);
-		vl_vmul_s (self->d1p, self->d1p, (vlf_t) 1.0 / (data[offset].time[0] - data[offset-1].time[0]));
-	}
-	else { vl_vzero(self->d1p); }
+	// assume fixed step for now
+	vlf_t h = obj->log_list[0x01].time[1];
 	
-	if (offset < (obj->log_offset-1))
-	{
-		vl_vsub (self->d1n, &data[offset+1].pos[0][0], &data[offset].pos[0][0]);
-		vl_vmul_s (self->d1n, self->d1n, (vlf_t) 1.0 / (data[offset+1].time[0] - data[offset].time[0]));
-	}
-	else { vl_vzero(self->d1n); }
+	if (offset >= 0x01) { vl_vcopy(&self->pos[0][0], &obj->log_list[offset-1].pos[0][0]); }
+	else  				{ vl_vinter(&self->pos[0][0], &obj->log_list[offset].pos[0][0], &obj->log_list[offset+1].pos[0][0], -1.0); }
 	
-	if 		(offset == 0x00) 				{ vl_vinter(&data[offset].pos[1][0], self->d1p, self->d1n, 1.0); }
-	else if (offset == (obj->log_offset-1)) { vl_vinter(&data[offset].pos[1][0], self->d1p, self->d1n, 0.0); }
-	else 									{ vl_vinter(&data[offset].pos[1][0], self->d1p, self->d1n, 0.5); }
+	vl_vcopy(&self->pos[1][0], &obj->log_list[offset].pos[0][0]);
 	
-	vl_vsub (self->d2, self->d1n, self->d1p);
-//
-//	if 		(offset == 0x00) 				{ vl_vmul_s(&data[offset].pos[2][0], self->d2, (vlf_t) 1.0 / (data[offset+1].time[0] - data[offset].time[0])); }
-//	else if (offset == (obj->log_offset-1)) { vl_vmul_s(&data[offset].pos[2][0], self->d2, (vlf_t) 1.0 / (data[offset].time[0] - data[offset-1].time[0])); }
-//	else 									{ vl_vmul_s(&data[offset].pos[2][0], self->d2, (vlf_t) 0.5 / (data[offset+1].time[0] - data[offset-1].time[0])); }
-
-	if 		(offset == 0x00) 				{ vl_vzero(&data[offset].pos[2][0]); }
-	else if (offset == (obj->log_offset-1)) { vl_vzero(&data[offset].pos[2][0]); }
-	else 									{ vl_vmul_s(&data[offset].pos[2][0], self->d2, (vlf_t) 2.0 / (data[offset+1].time[0] - data[offset-1].time[0])); }
+	if (offset <= obj->log_offset-2) { vl_vcopy(&self->pos[2][0], &obj->log_list[offset+1].pos[0][0]); }
+	else  							 { vl_vinter(&self->pos[2][0], &obj->log_list[offset-1].pos[0][0], &obj->log_list[offset].pos[0][0], 2.0); }
 	
-	if (offset > 0x00
-		&& fabs(vl_mdist(&data[offset-1].rot[0][0], &data[offset].rot[0][0])) > 1E-16)
-	{
-		vl_tnp(self->rnt, &data[offset-1].rot[0][0]);
-		vl_mmul_m(self->a, &data[offset].rot[0][0], self->rnt);
-		self->tra = vl_mtrace(self->a);
-		self->omg = acos(0.5*(self->tra - 1.0));
-		vl_tnp(self->at, self->a);
-		vl_msub(self->da, self->a, self->at);
-		
-		vl_mmul_s(self->d1rp, self->da,
-				  ( 0.5 / (data[offset].time[0] - data[offset-1].time[0]) )
-				  * ( self->omg / vl_sin(self->omg) ));
-	}
-	else { vl_mzero(self->d1rp); }
+	vl_vset(vel, 0.0);
+	vl_vsumm(vel, vel, &self->pos[0][0], -1.0 / (2.0 * h));
+	vl_vsumm(vel, vel, &self->pos[2][0], +1.0 / (2.0 * h));
 	
-	if (offset < (obj->log_offset-1)
-	&& fabs(vl_mdist(&data[offset].rot[0][0], &data[offset+1].rot[0][0])) > 1E-16)
-	{
-		vl_tnp(self->rnt, &data[offset].rot[0][0]);
-		vl_mmul_m(self->a, &data[offset+1].rot[0][0], self->rnt);
-		self->tra = vl_mtrace(self->a);
-		self->omg = acos(0.5*(self->tra - 1.0));
-		vl_tnp(self->at, self->a);
-		vl_msub(self->da, self->a, self->at);
-		
-		vl_mmul_s(self->d1rn, self->da,
-				  ( 0.5 / (data[offset+1].time[0] - data[offset].time[0]) )
-				  * ( self->omg / vl_sin(self->omg) ));
-	}
-	else { vl_mzero(self->d1rn); }
+	vl_vset(acc, 0.0);
+	vl_vsumm(acc, acc, &self->pos[0][0], +1.0 / (h * h));
+	vl_vsumm(acc, acc, &self->pos[1][0], -2.0 / (h * h));
+	vl_vsumm(acc, acc, &self->pos[2][0], +1.0 / (h * h));
 	
-	if 		(offset == 0x00) 				{ vl_minter(&data[offset].rot[1][0], self->d1rp, self->d1rn, 1.0); }
-	else if (offset == (obj->log_offset-1)) { vl_minter(&data[offset].rot[1][0], self->d1rp, self->d1rn, 0.0); }
-	else 									{ vl_minter(&data[offset].rot[1][0], self->d1rp, self->d1rn, 0.5); }
+	vlf_t *rot = &obj->log_list[offset].rot[1][0];
+	
+	if (offset >= 0x01) { vl_mcopy(&self->rot[0][0], &obj->log_list[offset-1].rot[0][0]); }
+	else  				{ vl_rinter(&self->rot[0][0], &obj->log_list[offset].rot[0][0], &obj->log_list[offset+1].rot[0][0], -1.0); }
+	
+	vl_mcopy(&self->rot[1][0], &obj->log_list[offset].rot[0][0]);
+	
+	if (offset <= obj->log_offset-2) { vl_mcopy(&self->rot[2][0], &obj->log_list[offset+1].rot[0][0]); }
+	else  							 { vl_rinter(&self->rot[2][0], &obj->log_list[offset-1].rot[0][0], &obj->log_list[offset].rot[0][0], +2.0); }
+	
+	vl_vset(rot, 0.0);
+	vl_rd1f(&self->rd1_data, rot, &self->rot[0][0], &self->rot[2][0]);
+	vl_mmul_s(rot, rot, 1.0 / (2.0 * h));
 	
 	// Error calculation
 	// if first log then set
@@ -144,7 +105,7 @@ inline uint8_t trj_proc_euler_update(s_trj_proc_euler *self, s_trj_obj *obj, uin
 		vlf_t tied_grs[9];
 		
 		vl_mtmul_v(tied_acc, &obj->log_list[offset-1].rot[0][0],
-				&obj->log_list[offset-1].pos[2][0]);
+				   &obj->log_list[offset-1].pos[2][0]);
 		
 		vl_mtmul_m(tied_grs, &obj->log_list[offset-1].rot[0][0],
 				   &obj->log_list[offset-1].rot[1][0]);
@@ -155,19 +116,13 @@ inline uint8_t trj_proc_euler_update(s_trj_proc_euler *self, s_trj_obj *obj, uin
 		vl_mmul_v(inert_acc, &obj->rot[0][0], tied_acc);
 		vl_mmul_m(inert_grs, &obj->rot[0][0], tied_grs);
 		
-//		vl_vcopy(inert_acc, &obj->log_list[offset-1].pos[2][0]);
-		
 		vl_mmul_m(inert_grs, inert_grs, &obj->rot[0][0]);
 		vl_msumm(&obj->rot[0][0], &obj->rot[0][0], inert_grs, obj->log_list[offset+1].time[1]);
 		vl_rnorm(&obj->rot[0][0]);
-//
-//		vl_vcopy(&obj->pos[1][0], &obj->log_list[offset-1].pos[1][0]);
-//		vl_vcopy(&obj->pos[2][0], &obj->log_list[offset-1].pos[2][0]);
-//
 		
 		vl_vsumm(&obj->pos[0][0], &obj->pos[0][0], &obj->pos[1][0], obj->log_list[offset].time[1]);
 		vl_vsumm(&obj->pos[0][0], &obj->pos[0][0], &obj->pos[2][0], obj->log_list[offset].time[1]*obj->log_list[offset].time[1]*0.5);
-
+		
 		vl_vsumm(&obj->pos[1][0], &obj->pos[1][0], inert_acc, obj->log_list[offset].time[1]);
 		
 		vl_vcopy(&obj->pos[2][0], inert_acc);
@@ -176,9 +131,6 @@ inline uint8_t trj_proc_euler_update(s_trj_proc_euler *self, s_trj_obj *obj, uin
 	// Calculate and write to pos/rot error L2 norm
 	obj->log_list[offset].pos_error = vl_vdist(&obj->log_list[offset].pos[0][0], &obj->pos[0][0]);
 	obj->log_list[offset].rot_error = vl_mdist(&obj->log_list[offset].rot[0][0], &obj->rot[0][0]);
-
-//	printf("%s %f \n", obj->desc, fabs(vl_mdist(&obj->log_list[offset].rot[0][0], &obj->rot[0][0])));
-//	fflush(stdout);
 	
 	return 0x00;
 }
@@ -294,26 +246,27 @@ inline uint8_t trj_proc_fps_update(s_trj_proc_fps *self, s_trj_obj *obj, uint32_
 	vlf_t *rot = &obj->log_list[offset].rot[1][0];
 	
 	if (offset >= 0x02) { vl_mcopy(&self->rot[0][0], &obj->log_list[offset-2].rot[0][0]); }
-	else  				{ vl_mcopy(&self->rot[0][0], &obj->log_list[offset].rot[0][0]); }
+	else  				{ vl_rinter(&self->rot[0][0], &obj->log_list[offset].rot[0][0], &obj->log_list[offset+2].rot[0][0], -1.0); }
 	
 	if (offset >= 0x01) { vl_mcopy(&self->rot[1][0], &obj->log_list[offset-1].rot[0][0]); }
-	else  				{ vl_mcopy(&self->rot[1][0], &obj->log_list[offset].rot[0][0]); }
+	else  				{ vl_rinter(&self->rot[1][0], &obj->log_list[offset].rot[0][0], &obj->log_list[offset+1].rot[0][0], -1.0); }
 	
-	vl_mcopy(&self->pos[2][0], &obj->log_list[offset].rot[0][0]);
+	vl_mcopy(&self->rot[2][0], &obj->log_list[offset].rot[0][0]);
 	
 	if (offset <= obj->log_offset-2) { vl_mcopy(&self->rot[3][0], &obj->log_list[offset+1].rot[0][0]); }
-	else  							 { vl_mcopy(&self->rot[3][0], &obj->log_list[offset].rot[0][0]); }
+	else  							 { vl_rinter(&self->rot[3][0], &obj->log_list[offset-1].rot[0][0], &obj->log_list[offset].rot[0][0], +2.0); }
 	
 	if (offset <= obj->log_offset-3) { vl_mcopy(&self->rot[4][0], &obj->log_list[offset+2].rot[0][0]); }
-	else  							 { vl_mcopy(&self->rot[4][0], &obj->log_list[offset].rot[0][0]); }
+	else  							 { vl_rinter(&self->rot[4][0], &obj->log_list[offset-2].rot[0][0], &obj->log_list[offset].rot[0][0], +2.0); }
 	
 	// f_x = (1*f[i-2]-8*f[i-1]+0*f[i+0]+8*f[i+1]-1*f[i+2])/(12*1.0*h**1)
 	// f_x = (8*(f[i+1]-f[i-1])    -(f[i+2]-f[i-2]))/(12*1.0*h**1)
 	vl_vset(rot, 0.0);
 	vl_rd1f(&self->rd1_data, &self->rd1[0][0], &self->rot[1][0], &self->rot[3][0]);
 	vl_rd1f(&self->rd1_data, &self->rd1[1][0], &self->rot[0][0], &self->rot[4][0]);
-	vl_msumm(rot, rot, &self->rd1[0][0], +8.0 / (12.0 * h));
-	vl_msumm(rot, rot, &self->rd1[1][0], -1.0 / (12.0 * h));
+	vl_mmul_s(&self->rd1[0][0], &self->rd1[0][0], +8.0 / (12.0 * h));
+	vl_mmul_s(&self->rd1[1][0], &self->rd1[1][0], -1.0 / (12.0 * h));
+	vl_msum(rot, &self->rd1[0][0], &self->rd1[1][0]);
 	
 	// Error calculation
 	// if first log then set
@@ -361,9 +314,6 @@ inline uint8_t trj_proc_fps_update(s_trj_proc_fps *self, s_trj_obj *obj, uint32_
 	obj->log_list[offset].pos_error = vl_vdist(&obj->log_list[offset].pos[0][0], &obj->pos[0][0]);
 	obj->log_list[offset].rot_error = vl_mdist(&obj->log_list[offset].rot[0][0], &obj->rot[0][0]);
 
-//	printf("%s %f \n", obj->desc, fabs(vl_mdist(&obj->log_list[offset].rot[0][0], &obj->rot[0][0])));
-//	fflush(stdout);
-	
 	return 0x00;
 }
 
