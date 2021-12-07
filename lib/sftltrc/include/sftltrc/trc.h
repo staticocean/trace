3d596b1cc500d09e54047f7d1e32a828d3f9b180
johnbrown <egorchekhov@icloud.com>
2021-12-07 16:46:07 +0300

    [P] [sftltrc] restructured sftltrc

---
+++ lib/sftltrc/include/sftltrc/trc.h
@@ -0,0 +1,26 @@
+
+// 2021 Softael LLC.
+// Created by staticocean on 11/11/2021
+
+// trc - trace
+
+#ifndef __TRC__
+#define __TRC__
+
+//------------------------------------------------------------------------------
+
+#include <sftltrc/trcctrl.h>
+#include <sftltrc/trcdata.h>
+#include <sftltrc/trcellp.h>
+#include <sftltrc/trceng.h>
+#include <sftltrc/trcobj.h>
+#include <sftltrc/trcproc.h>
+#include <sftltrc/trcspl.h>
+#include <sftltrc/trctraj.h>
+#include <sftltrc/trcpack.h>
+
+//------------------------------------------------------------------------------
+
+#endif /* __TRC__ */
+
+
